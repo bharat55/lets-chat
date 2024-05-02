adff01b0c26a51f0472957f31f5f036374bce7ea
Bharat Rathor <bharat.rathor@gemsessence.com>
2024-05-02 15:23:14 +0530

    Google Auth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import { ThemeProvider } from "../components/ThemeProvider"
+import ClientProvider from "@/components/ClientProvider";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,18 +20,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-         attribute="class"
-         defaultTheme="system"
-         enableSystem
-         disableTransitionOnChange
-        >
-          <Header></Header>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    
+      <html lang="en">
+          <body className={inter.className}>
+          <ClientProvider>
+            <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            >
+              <Header></Header>
+              {children}
+            </ThemeProvider>
+          </ClientProvider>
+          </body>
+      </html>
+    
   );
 }
---
+++ auth.ts
@@ -0,0 +1,12 @@
+import { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.CLIENT_ID!,
+      clientSecret: process.env.CLIENT_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+} satisfies NextAuthOptions;
